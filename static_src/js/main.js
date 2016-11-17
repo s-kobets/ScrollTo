8ea285e428a84282f663411e3f0c7ca677fb70c7
Sergey <s-kobets617@yandex.ru>
2016-11-17 23:37:43 +0300

    in object ScrollTo

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -1,110 +1,125 @@
-window.addEventListener('resize', function () {
-	scrollTo();
-});
+(function () {
 
-scrollTo();
+	var ScrollTo = {
+		anchors: [],
+		currentAnchor: 0,
+		isAnimating: false,
+		_init: function () {
+			console.log(this);
+			this._updateAnchors();
+			this._curentAnchorTo();
+		},
 
-function scrollTo() {
-	var anchors = [];
-	var currentAnchor = 0;
-	var isAnimating  = false;
+		_updateAnchors: function () {
+			var anchors = [];
+			var elements = document.querySelectorAll('.anchor');
+			Array.prototype.forEach.call(elements, function(el, i){
+				anchors.push(el.offsetTop);
+			});
+			this.anchors = anchors;
+		},
 
-	updateAnchors();
-	curentAnchorTo();
+		_curentAnchorTo: function () {
+			// reload curentAnchor
+			var anchors = this.anchors;
+			var currentAnchor = this.currentAnchor;
+			var heightWindow = screen.height/2;
+			var curentReload = window.pageYOffset;
+			var index = 0;
 
-	function updateAnchors() {
-		anchors = [];
-		var elements = document.querySelectorAll('.anchor');
-		Array.prototype.forEach.call(elements, function(el, i){
-			anchors.push(el.offsetTop);
-		});
-		console.log(anchors);
-	}
-
-	function curentAnchorTo() {
-		// reload curentAnchor
-		var heightWindow = screen.height/2;
-		var curentReload = window.pageYOffset;
-		var index = 0;
-		if (curentReload > anchors[anchors.length - 1]) {
-			curentReload = anchors[anchors.length - 1];
-		}
-
-		if (anchors.indexOf(curentReload) !== -1) {
-			currentAnchor = anchors.indexOf(curentReload);
-		} else {
-			for (var i = 0; i < anchors.length; i++) {
-				if (anchors[i] > curentReload && anchors[i-1] < curentReload) {
-					index = i;
-				}
+			console.log(currentAnchor, anchors);
+			if (curentReload > anchors[anchors.length - 1]) {
+				curentReload = anchors[anchors.length - 1];
 			}
 
-			if ((anchors[index] - curentReload) > heightWindow) {
-				currentAnchor = index - 1;
+			if (anchors.indexOf(curentReload) !== -1) {
+				currentAnchor = anchors.indexOf(curentReload);
 			} else {
-				currentAnchor = index;
-			}
-		}
-		console.log('при скроле скролом', currentAnchor, heightWindow, curentReload);
-	}
-	
-	// For Chrome
-	window.addEventListener('mousewheel', blockScroll);
-
-	// For Firefox
-	window.addEventListener('DOMMouseScroll', blockScroll);
-
-	function blockScroll(e) { 
-		e.preventDefault();
-		e.stopPropagation();
-
-		curentAnchorTo();
-		console.log(isAnimating);
-
-		if (isAnimating) {
-			return false;
-		}
-
-		isAnimating  = true;
-		// Increase or reset current anchor
-		var delta = e.wheelDelta ? e.wheelDelta : -e.detail;
-		if (delta >= 0) {
-			currentAnchor--;
-		} else {
-			currentAnchor++;
-		}
-
-		if (currentAnchor > (anchors.length - 1)) {
-			currentAnchor = anchors.length - 1;
-		} else if (currentAnchor < 0 ) {
-			currentAnchor = 0;
-		}
-		console.log('к нему крутим', currentAnchor);
-
-		var timePassed = window.pageYOffset;
-		var finish = parseInt(anchors[currentAnchor], 10);
-
-		var timer = setInterval( function () {
-			if (finish >= timePassed) {
-				timePassed += 10;
-				if (timePassed > finish) {
-					clearInterval(timer);
-	    			return;
+				for (var i = 0; i < anchors.length; i++) {
+					if (anchors[i] > curentReload && anchors[i-1] < curentReload) {
+						index = i;
+					}
 				}
-				window.scroll(0, timePassed);
 
+				if ((anchors[index] - curentReload) > heightWindow) {
+					currentAnchor = index - 1;
+				} else {
+					currentAnchor = index;
+				}
+			}
+			this.currentAnchor = currentAnchor;
+			console.log('при скроле скролом', currentAnchor, heightWindow, curentReload);
+		},
+
+		_blockScroll: function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			isAnimating = this.isAnimating;
+			currentAnchor = this.currentAnchor;
+			anchors = this.anchors;
+
+			this._curentAnchorTo();
+			console.log(isAnimating, 'текущий курент', currentAnchor, anchors);
+
+			if (isAnimating) {
+				return false;
+			}
+
+			isAnimating  = true;
+			// Increase or reset current anchor
+			var delta = e.wheelDelta ? e.wheelDelta : -e.detail;
+			if (delta >= 0) {
+				currentAnchor--;
 			} else {
-				timePassed -= 10;
-				if (timePassed < finish) {
-					clearInterval(timer);
-	    			return;
-				}
-				window.scroll(0, timePassed);
+				currentAnchor++;
 			}
-		}, 20);
 
-		isAnimating  = false;
+			if (currentAnchor > (anchors.length - 1)) {
+				currentAnchor = anchors.length - 1;
+			} else if (currentAnchor < 0 ) {
+				currentAnchor = 0;
+			}
+			console.log('к нему крутим', currentAnchor);
+
+			var timePassed = window.pageYOffset;
+			var finish = parseInt(anchors[currentAnchor], 10);
+
+			console.log('start=', timePassed, finish);
+			var timer = setInterval( function () {
+				if (finish >= timePassed) {
+					timePassed += 10;
+					if (timePassed > finish) {
+						clearInterval(timer);
+		    			return;
+					}
+					window.scroll(0, timePassed);
+
+				} else {
+					timePassed -= 10;
+					if (timePassed < finish) {
+						clearInterval(timer);
+		    			return;
+					}
+					window.scroll(0, timePassed);
+				}
+			}, 20);
+
+			this.isAnimating = isAnimating = false;
+		}
 	};
 
-	updateAnchors();
-}
+	ScrollTo._init();
+
+		// For Chrome
+	window.addEventListener('mousewheel', ScrollTo._blockScroll.bind(ScrollTo));
+
+	// For Firefox
+	window.addEventListener('DOMMouseScroll', ScrollTo._blockScroll.bind(ScrollTo));
+
+	window.addEventListener('resize', function () {
+		ScrollTo._updateAnchors();
+	});
+
+
+})();
