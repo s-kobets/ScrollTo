c7e71d1049a7442d7197a7807fba85a79e8a0162
Sergey <s-kobets617@yandex.ru>
2016-11-18 17:22:13 +0300

    webpack + 100% work

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -1,126 +1,123 @@
-(function () {
+var ScrollTo = {
+	anchors: [],
+	currentAnchor: 0,
+	isAnimating: false,
+	_init: function () {
+		console.log(this);
+		this._updateAnchors();
+		this._curentAnchorTo();
+	},
 
-	var ScrollTo = {
-		anchors: [],
-		currentAnchor: 0,
-		isAnimating: false,
-		_init: function () {
-			console.log(this);
-			this._updateAnchors();
-			this._curentAnchorTo();
-		},
+	_updateAnchors: function () {
+		let anchors = [];
+		const elements = document.querySelectorAll('.anchor');
+		Array.prototype.forEach.call(elements, function(el, i){
+			anchors.push(el.offsetTop);
+		});
+		this.anchors = anchors;
+	},
 
-		_updateAnchors: function () {
-			var anchors = [];
-			var elements = document.querySelectorAll('.anchor');
-			Array.prototype.forEach.call(elements, function(el, i){
-				anchors.push(el.offsetTop);
-			});
-			this.anchors = anchors;
-		},
+	_curentAnchorTo: function () {
+		// reload curentAnchor
+		let anchors = this.anchors;
+		let currentAnchor = this.currentAnchor;
+		let heightWindow = screen.height/2;
+		let curentReload = window.pageYOffset;
+		let index = 0;
 
-		_curentAnchorTo: function () {
-			// reload curentAnchor
-			var anchors = this.anchors;
-			var currentAnchor = this.currentAnchor;
-			var heightWindow = screen.height/2;
-			var curentReload = window.pageYOffset;
-			var index = 0;
-
-			console.log(currentAnchor, anchors);
-			if (curentReload > anchors[anchors.length - 1]) {
-				curentReload = anchors[anchors.length - 1];
-			}
-
-			if (anchors.indexOf(curentReload) !== -1) {
-				currentAnchor = anchors.indexOf(curentReload);
-			} else {
-				for (var i = 0; i < anchors.length; i++) {
-					if (anchors[i] > curentReload && anchors[i-1] < curentReload) {
-						index = i;
-					}
-				}
-
-				if ((anchors[index] - curentReload) > heightWindow) {
-					currentAnchor = index - 1;
-				} else {
-					currentAnchor = index;
-				}
-			}
-			this.currentAnchor = currentAnchor;
-			console.log('при скроле скролом', currentAnchor, heightWindow, curentReload);
-		},
-
-		_blockScroll: function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-
-			isAnimating = this.isAnimating;
-			currentAnchor = this.currentAnchor;
-			anchors = this.anchors;
-
-			this._curentAnchorTo();
-			console.log(isAnimating, 'текущий курент', currentAnchor, anchors);
-
-			if (isAnimating) {
-				return false;
-			}
-
-			isAnimating  = true;
-			// Increase or reset current anchor
-			var delta = e.wheelDelta ? e.wheelDelta : -e.detail;
-			if (delta >= 0) {
-				currentAnchor--;
-			} else {
-				currentAnchor++;
-			}
-
-			if (currentAnchor > (anchors.length - 1)) {
-				currentAnchor = anchors.length - 1;
-			} else if (currentAnchor < 0 ) {
-				currentAnchor = 0;
-			}
-			console.log('к нему крутим', currentAnchor);
-
-			var timePassed = window.pageYOffset;
-			var finish = parseInt(anchors[currentAnchor], 10);
-
-			console.log('start=', timePassed, finish);
-			var timer = setInterval( function () {
-				if (finish >= timePassed) {
-					timePassed += 10;
-					if (timePassed > finish) {
-						clearInterval(timer);
-		    			return;
-					}
-					window.scroll(0, timePassed);
-
-				} else {
-					timePassed -= 10;
-					if (timePassed < finish) {
-						clearInterval(timer);
-		    			return;
-					}
-					window.scroll(0, timePassed);
-				}
-			}, 20);
-
-			this.currentAnchor = currentAnchor;
-			this.isAnimating = isAnimating = false;
+		console.log(currentAnchor, anchors);
+		if (curentReload > anchors[anchors.length - 1]) {
+			curentReload = anchors[anchors.length - 1];
 		}
-	};
 
-	ScrollTo._init();
+		if (anchors.indexOf(curentReload) !== -1) {
+			currentAnchor = anchors.indexOf(curentReload);
+		} else {
+			for (let i = 0; i < anchors.length; i++) {
+				if (anchors[i] > curentReload && anchors[i-1] < curentReload) {
+					index = i;
+				}
+			}
 
-		// For Chrome
-	window.addEventListener('mousewheel', ScrollTo._blockScroll.bind(ScrollTo));
+			if ((anchors[index] - curentReload) > heightWindow) {
+				currentAnchor = index - 1;
+			} else {
+				currentAnchor = index;
+			}
+		}
+		this.currentAnchor = currentAnchor;
+		console.log('при скроле скролом', currentAnchor, heightWindow, curentReload);
+	},
 
-	// For Firefox
-	window.addEventListener('DOMMouseScroll', ScrollTo._blockScroll.bind(ScrollTo));
+	_blockScroll: function (e) {
+		e.preventDefault();
+		e.stopPropagation();
 
-	window.addEventListener('resize', function () {
-		ScrollTo._updateAnchors();
-	});
+		let isAnimating = this.isAnimating;
+		let currentAnchor = this.currentAnchor;
+		let anchors = this.anchors;
 
+		this._curentAnchorTo();
+		console.log(isAnimating, 'текущий курент', currentAnchor, anchors);
 
-})();
+		if (isAnimating) {
+			return false;
+		}
+
+		isAnimating  = true;
+		// Increase or reset current anchor
+		var delta = e.wheelDelta ? e.wheelDelta : -e.detail;
+		if (delta >= 0) {
+			currentAnchor--;
+		} else {
+			currentAnchor++;
+		}
+
+		if (currentAnchor > (anchors.length - 1)) {
+			currentAnchor = anchors.length - 1;
+		} else if (currentAnchor < 0 ) {
+			currentAnchor = 0;
+		}
+		console.log('к нему крутим', currentAnchor);
+
+		var timePassed = window.pageYOffset;
+		var finish = parseInt(anchors[currentAnchor], 10);
+
+		console.log('start=', timePassed, finish);
+		let timer = setInterval( function () {
+			if (finish >= timePassed) {
+				timePassed += 10;
+				if (timePassed > finish) {
+					clearInterval(timer);
+	    			return;
+				}
+				window.scroll(0, timePassed);
+
+			} else {
+				timePassed -= 10;
+				if (timePassed < finish) {
+					clearInterval(timer);
+	    			return;
+				}
+				window.scroll(0, timePassed);
+			}
+		}, 20);
+
+		this.currentAnchor = currentAnchor;
+		this.isAnimating = isAnimating = false;
+	}
+};
+
+ScrollTo._init();
+
+	// For Chrome
+window.addEventListener('mousewheel', ScrollTo._blockScroll.bind(ScrollTo));
+
+// For Firefox
+window.addEventListener('DOMMouseScroll', ScrollTo._blockScroll.bind(ScrollTo));
+
+window.addEventListener('resize', function () {
+	ScrollTo._updateAnchors();
+});
+
+export default ScrollTo;
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+
+//определение переменной окружения
+const NODE_ENV = process.env.NODE_ENV || "development";
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./static_src/js/main.js",
+    output: {
+        path: __dirname + "/public/js",
+        filename: "main.js"
+    },
+
+    watch: NODE_ENV == "development",
+
+    //ждет 100мс а потом запускает сборку во время watch
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == "development" ? "cheap-module-inline-source-map" : null,
+
+    plugins:[
+        new webpack.DefinePlugin({ //делаем ключ NODE_ENV доступный клиенту
+            NODE_ENV : JSON.stringify(NODE_ENV),  //делает строковым типом
+            LANG     : JSON.stringify('ru')
+        })  
+    ],
+
+    resolve: { //указывает где искать модули, например ./static_src/scripts/main.js
+        modulesDirectories: ['node_modules'],
+        extensions: ['','.js']
+    },
+
+    resolveLoader: { //указывает где искать модули для loaders например babel
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader', '*'],
+        extensions: ['','.js']
+    },
+
+    module: { //устанавливаем нужные модули -- npm i имя
+        loaders: [{
+            test: /\.(js|jsx)$/,   //файлы заканчивающиеся на .js, применим babel      
+            loader: "babel"
+            // ,exclude: /(parsleyjs)/
+        }]
+    }
+
+};
+
+if (NODE_ENV == 'production'){  // для минификации кода
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,  //не нужно предупреждений
+                drop_console: true,  //убрать сonsole.log
+                unsafe: true  //небезопасные штуки использовать
+            }
+        })
+    );
+}
+    
+
+
